c5581626f76860c05a5c372854246af1a8588e4f
tbosman <tbosman@gmail.com>
2014-09-16 11:32:59 +0200

    ##SCORE## function1 9.316807054442119 function2 4.945555695210683 function3 6.8854848556631865

--- evocomputing2014team4/src/org/vu/evocomputing2014team4/algorithms/buildingblocks/ParameterisedMutator.java
+++ evocomputing2014team4/src/org/vu/evocomputing2014team4/algorithms/buildingblocks/ParameterisedMutator.java
@@ -122,7 +122,7 @@ public class ParameterisedMutator implements Mutator {
 			
 			for(int i=0; i < genome.value.length; i++) {
 
-				newValue[i] =  genome.value[i] + temp*randSamp[i]*Math.pow(0.1, genome.precision-1);
+				newValue[i] =  genome.value[i] + temp*randSamp[i];
 				if(Math.abs(newValue[i]) > 5) {
 					double outOfBoundary = Math.abs(newValue[i]) - 5;//'Bounce' of the walls
 					newValue[i] = Math.signum(newValue[i])*(5 - outOfBoundary);
